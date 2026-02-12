b0c9cb30ee678ddea4102cc390c82ff71936577d
Sagar Kumar <sagar@prayas.ai>
2026-02-12 14:11:51 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "true"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+// "89" => 89
+// "89abc" => NaN
+// true => 1
+// false => 0
+
+
+let isLoggedIn = "sagar"
+
+let booleanISLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanISLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "sagar" => true  
+
+
+let someNumber = 89
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
+
+console.log(typeof stringNumber)
