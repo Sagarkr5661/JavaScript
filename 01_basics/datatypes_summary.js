11994c7b1115255aa2ee4f03da6522a894b658c0
Sagar Kumar <sagar@prayas.ai>
2026-02-16 13:38:44 +0530

    add Memory(Stack,Heap)

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,41 +1,70 @@
-// # Primitve datatypes 
+// // # Primitve datatypes 
 
-// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 10
-const scoreValue = 10.3
+// const score = 10
+// const scoreValue = 10.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;  // undefined
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;  // undefined
 
-const id = Symbol("123")
-const anotherId = Symbol("123")
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
 
-console.log(id === anotherId);  // false because symbol is unique
+// console.log(id === anotherId);  // false because symbol is unique
 
-const bigNumber = 453532342121434n  // n at the end is used to denote that this is a bigInt
-
-
+// const bigNumber = 453532342121434n  // n at the end is used to denote that this is a bigInt
 
 
-// Reference Types (Non-primitive datatypes)
-
-// Array, Objects, Functions
 
 
-const heros = ["Ironman", "Spiderman", "Thor"]  // array
-let myObj = {
-    name: "Sagar",  // object
-    age: 22,
-    isLoggedIn: false
+// // Reference Types (Non-primitive datatypes)
+
+// // Array, Objects, Functions
+
+
+// const heros = ["Ironman", "Spiderman", "Thor"]  // array
+// let myObj = {
+//     name: "Sagar",  // object
+//     age: 22,
+//     isLoggedIn: false
+// }
+
+
+// const myFunction = function(){
+//     console.log("Hello world")
+// }
+
+// console.log(typeof heros);
+// console.log(typeof myObj);
+// console.log(typeof myFunction);
+
+
+
+
+
+
+
+// ****************** Memomry ******************
+
+// Stack(Ptimitive), Heap(Non-primitive)
+
+let myName = "Sagar Kumar" // stored in stack/primitive
+let anotherName = "Sagar" // stored in stack/primitiv
+
+console.log(anotherName);
+console.log(myName);
+
+let userOne = {
+    email : "sagarkumar@gmail.com",
+    upi : "sagar@axl"
+ 
 }
 
+let userTwo = userOne  // stored in heap/non-primitive
 
-const myFunction = function(){
-    console.log("Hello world")
-}
+userTwo.email = "sagar2@gmail.com"
 
-console.log(typeof heros);
-console.log(typeof myObj);
-console.log(typeof myFunction);
+console.log(userOne.email);
+console.log(userTwo.email);
